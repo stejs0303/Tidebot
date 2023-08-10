ab0864b3de9a3d6ce96f0b586a9eb29c8a530300
Jan Stejskal <janstej1@gmail.com>
2023-08-10 21:25:57 +0200

    preparations for implementation

--- modules/gear.py
+++ modules/gear.py
@@ -1,6 +1,28 @@
 import discord
 from discord.ext import commands
 
+from config import cfg
+from database import db
+
+""" SAVED IN DATABASE:
+        user_id INTEGER PRIMARY KEY, 
+        ap INTEGER, 
+        aap INTEGER,
+        dp INTEGER,
+        health INTEGER, 
+        all_ap INTEGER,
+        all_aap INTEGER,
+        accuracy INTEGER, 
+        dr TEXT,
+        dr_rate INTEGER, 
+        evasion TEXT,
+        se_rate INTEGER,
+        class TEXT, 
+        level REAL,
+        gear_planner TEXT,
+        gear_image INTEGER 
+"""
+
 class Gear(commands.Cog):
     def __init__(self, bot: commands.Bot):
         self.bot = bot
--- modules/memes.py
+++ modules/memes.py
@@ -1,6 +1,11 @@
 import discord
 from discord.ext import commands
 
+from config import cfg
+from database import db
+
+
+
 class Memes(commands.Cog):
     def __init__(self, bot: commands.Bot):
         self.bot = bot
