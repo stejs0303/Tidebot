4adc55ccc1184f7709a3f69845dd581488eca852
Jan Stejskal <janstej1@gmail.com>
2023-08-15 19:16:45 +0200

    rename config dict to ranking/gear

--- config.py
+++ config.py
@@ -4,41 +4,41 @@ from PIL import ImageFont
 class _Ranking:
     # TODO: Add __slots__()
     
-    def __init__(self, config: dict) -> None:
-        self.level_scaling              = config["SCALING"]
-        self.img_width                  = config["IMG_WIDTH"]
-        self.img_height                 = config["IMG_HEIGHT"]
-        self.icon_width                 = config["ICON_WIDTH"]
-        self.icon_height                = config["ICON_HEIGHT"]
-        self.w_offset                   = config["W_OFFSET"]
-        self.h_offset                   = config["H_OFFSET"]
-        self.bg_round_rad               = config["BG_ROUND_RAD"]
-        self.bar_round_rad              = config["BAR_ROUND_RAD"]
-        self.icon_round_rad             = config["ICON_ROUND_RAD"]
-        self.font_size_name             = config["FONT_LARGE"]
-        self.font_size_text             = config["FONT_SMALLER"]
+    def __init__(self, ranking: dict) -> None:
+        self.level_scaling              = ranking["SCALING"]
+        self.img_width                  = ranking["IMG_WIDTH"]
+        self.img_height                 = ranking["IMG_HEIGHT"]
+        self.icon_width                 = ranking["ICON_WIDTH"]
+        self.icon_height                = ranking["ICON_HEIGHT"]
+        self.w_offset                   = ranking["W_OFFSET"]
+        self.h_offset                   = ranking["H_OFFSET"]
+        self.bg_round_rad               = ranking["BG_ROUND_RAD"]
+        self.bar_round_rad              = ranking["BAR_ROUND_RAD"]
+        self.icon_round_rad             = ranking["ICON_ROUND_RAD"]
+        self.font_size_name             = ranking["FONT_LARGE"]
+        self.font_size_text             = ranking["FONT_SMALLER"]
         self.font_name                  = ImageFont.truetype("data\Montserrat-Bold.ttf", self.font_size_name)
         self.font_text                  = ImageFont.truetype("data\Montserrat-Bold.ttf", self.font_size_text)
-        self.default_bg_color           = config["DEFAULT_BG_COLOR"]
-        self.default_full_bar_color     = config["DEFAULT_FULL_BAR_COLOR"]
-        self.default_progress_bar_color = config["DEFAULT_PROGRESS_BAR_COLOR"]
-        self.default_text_color         = config["DEFAULT_TEXT_COLOR"]
-        self.path                       = config["BACKGROUND_FOLDER"]
+        self.default_bg_color           = ranking["DEFAULT_BG_COLOR"]
+        self.default_full_bar_color     = ranking["DEFAULT_FULL_BAR_COLOR"]
+        self.default_progress_bar_color = ranking["DEFAULT_PROGRESS_BAR_COLOR"]
+        self.default_text_color         = ranking["DEFAULT_TEXT_COLOR"]
+        self.path                       = ranking["BACKGROUND_FOLDER"]
 
 
 class _Gear:
     #TODO: Add __slots__()
     
-    def __init__(self, config: dict) -> None:
-        self.browser_window_width       = config["BROWSER_WINDOW_WIDTH"]
-        self.browser_window_height      = config["BROWSER_WINDOW_HEIGHT"]
-        self.browser_exec_path          = config["BROWSER_EXEC_PATH"]
-        self.img_x                      = config["IMG_X"]
-        self.img_y                      = config["IMG_Y"]
-        self.img_width                  = config["IMG_WIDTH"]
-        self.img_height                 = config["IMG_HEIGHT"]
-        self.extra_stats                = config["EXTRA_STATS"]
-        self.path                       = config["IMG_FOLDER"]
+    def __init__(self, gear: dict) -> None:
+        self.browser_window_width       = gear["BROWSER_WINDOW_WIDTH"]
+        self.browser_window_height      = gear["BROWSER_WINDOW_HEIGHT"]
+        self.browser_exec_path          = gear["BROWSER_EXEC_PATH"]
+        self.img_x                      = gear["IMG_X"]
+        self.img_y                      = gear["IMG_Y"]
+        self.img_width                  = gear["IMG_WIDTH"]
+        self.img_height                 = gear["IMG_HEIGHT"]
+        self.extra_stats                = gear["EXTRA_STATS"]
+        self.path                       = gear["IMG_FOLDER"]
 
 
 class Config:
@@ -73,5 +73,7 @@ class Config:
             print(e); exit(1)
             
         return cls(config)
+
+
 
 cfg = Config.from_json()
