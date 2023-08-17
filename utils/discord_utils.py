e1cc8d2233145e6ae2269769f4e6c659e44c0cdc
Jan Stejskal <janstej1@gmail.com>
2023-08-17 18:35:27 +0200

    Extracting getting image and saving image

--- utils/discord_utils.py
+++ utils/discord_utils.py
@@ -1,6 +1,21 @@
-from discord.ext import commands
+from discord.ext import commands as _commands
+import discord as _discord
 
-import re
+from PIL import Image as _Image
 
-def get_user(ctx: commands.Context, args) -> str:
-    return str(ctx.author.id) if not bool(args) else re.search("\d+", args[0]).group()
+import requests as _requests
+
+import re as _re
+import io as _io
+
+def get_user(ctx: _commands.Context, args) -> str:
+    return str(ctx.author.id) if not bool(args) else _re.search("\d+", args[0]).group()
+
+
+async def get_image_from_attachment(file: _discord.Attachment) -> None:
+    return _Image.open(_io.BytesIO(await file.read()))
+
+    
+def get_image_from_url(file: str) -> None:
+    return _Image.open(_requests.get(file, stream=True).raw)
+    
--- utils/image_processing.py
+++ utils/image_processing.py
@@ -1,10 +1,14 @@
-from PIL import Image, ImageDraw
+from PIL import Image as _Image 
+from PIL import ImageDraw as _ImageDraw
+from PIL.Image import Resampling as _Resampling
+
+from config import cfg as _cfg
 
 
 def round_corner(radius, fill):
     """Draw a round corner"""
-    corner = Image.new('RGBA', (radius, radius), 0x00000000)
-    draw = ImageDraw.Draw(corner)
+    corner = _Image.new('RGBA', (radius, radius), 0x00000000)
+    draw = _ImageDraw.Draw(corner)
     draw.pieslice((0, 0, radius * 2, radius * 2), 180, 270, fill=fill)
     return corner
 
@@ -12,10 +16,14 @@ def round_corner(radius, fill):
 def round_rectangle(size, radius, fill):
     """Draw a rounded rectangle"""
     width, height = size
-    rectangle = Image.new('RGBA', size, fill)
+    rectangle = _Image.new('RGBA', size, fill)
     corner = round_corner(radius, fill)
     rectangle.paste(corner, (0, 0))
     rectangle.paste(corner.rotate(90), (0, height - radius))  # Rotate the corner and paste it
     rectangle.paste(corner.rotate(180), (width - radius, height - radius))
     rectangle.paste(corner.rotate(270), (width - radius, 0))
     return rectangle
+
+def resize_and_save(img: _Image.Image, img_path: str):
+    img = img.thumbnail((_cfg.ranking.img_width, _cfg.ranking.img_height), _Resampling.BICUBIC)
+    img.save(img_path)
