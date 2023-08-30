9487aa6b492f2c99d80ca5837b65de63bc4f7a70
Jan Stejskal <janstej1@gmail.com>
2023-08-30 18:47:09 +0200

    added new regex

--- compiled_regex.py
+++ compiled_regex.py
@@ -4,13 +4,15 @@ import re
 class _Regex:
     # TODO: Add __slots__()
     def __init__(self) -> None:
-        self.hexa_hash          = re.compile(r"^#[0-9a-f]{6}$", flags=re.RegexFlag.MULTILINE)
-        self.hexa_0x            = re.compile(r"^0x[0-9a-f]{6}$", flags=re.RegexFlag.MULTILINE)
-        self.int_pattern        = re.compile(r"^\d+$", flags=re.RegexFlag.MULTILINE)
-        self.percentage         = re.compile(r"^\d+((\.|,)\d+)?\ ?%?$", flags=re.RegexFlag.MULTILINE)
-        self.garmoth_url        = re.compile(r"^(https:\/\/|http:\/\/|www.)garmoth.com\/character\/[0-9a-zA-Z]{10}$", 
-                                             flags=re.RegexFlag.MULTILINE)
-        self.emote              = re.compile(r"^<:[a-zA-Z0-9]+:\d+>$", flags=re.RegexFlag.MULTILINE)
+        self.hexa_hash          = re.compile(r"^#[0-9a-f]{6}$")
+        self.hexa_0x            = re.compile(r"^0x[0-9a-f]{6}$")
+        self.int_pattern        = re.compile(r"^\d+$")
+        self.percentage         = re.compile(r"^\d+((\.|,)\d+)?\ ?%?$")
+        self.garmoth_url        = re.compile(r"^(https:\/\/|http:\/\/|www.)garmoth.com\/character\/[0-9a-zA-Z]{10}$")
+        self.emote              = re.compile(r"^<:[a-zA-Z0-9]+:\d+>$")
+        self.tripplet           = re.compile(r"^(\( ?\d+, ?\d+, ?\d+ ?\))$")
+        self.string_or_level    = re.compile(r"(^[a-zA-z]+$)|(^\d+(.|,)\d+$)")
+
 
 
 regex = _Regex()
